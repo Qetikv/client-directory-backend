13455aa187744e79653775d12673077e36cc08b1
unknown <qqetikv@gmail.com>
2024-01-07 19:47:45 +0400

    server configuration

--- delete-file.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.delete('http://localhost:3000/api/users/clear')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error('Error clearing user data:', error.message);
-  });
